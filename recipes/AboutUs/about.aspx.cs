18286c64f5885b97d63b608f342beff098a2dd3b
svphiia5 <sal.bluehair@gmail.com>
2026-02-12 22:17:06 +0200

    Update about.aspx.cs

---
+++ recipes/AboutUs/about.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SRecipes
+{
+    public partial class About : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
